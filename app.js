633f252ba72735df8e316ad5e077fc6fd3bf8748
johndward01 <jackwade464@gmail.com>
2021-05-24 13:34:42 -0500

    created fn() and finished exercise

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(number) {
+    return (plusNumber => number + plusNumber)
+}
+
+let plus15 = plus(15);
+console.log(plus15(10));
